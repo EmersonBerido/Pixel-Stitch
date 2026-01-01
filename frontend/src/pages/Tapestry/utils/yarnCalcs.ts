d4e9f64e47351483dc165cdf156e15d6679b3940
EmersonBerido <beridoemerson05@gmail.com>
2026-01-01 13:59:19 -0800

    Personal measurements for each yarn size

--- frontend/src/pages/Tapestry/utils/yarnCalcs.ts
+++ frontend/src/pages/Tapestry/utils/yarnCalcs.ts
@@ -1,5 +1,6 @@
-// TODO: yarns 0-3, 5-7
-
+// Types
+export type Stitch = "sc" | "hdc" | "dc" | "tc" ;
+export type YarnSize = 0|1|2|3|4|5|6|7;
 export type Calculations = {
   sc : number,
   hdc : number,
@@ -7,32 +8,30 @@ export type Calculations = {
   tc : number
 }
 
-export type Stitch = "sc" | "hdc" | "dc" | "tc" ;
-export type YarnSize = 0|1|2|3|4|5|6|7;
-
+// Personal Research
 const yarn0 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 0.75,
+  hdc : 1.25,
+  dc : 2.75,
+  tc : 4.25
 }
 const yarn1 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 1,
+  hdc : 1.5,
+  dc : 3.25,
+  tc : 4.75
 }
 const yarn2 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 1.25,
+  hdc : 1.75,
+  dc : 3.75,
+  tc : 5.25
 }
 const yarn3 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 2,
+  hdc : 2.25,
+  dc : 4.25,
+  tc : 5.25
 }
 const yarn4 : Calculations = {
   sc : 2.25,
@@ -41,22 +40,22 @@ const yarn4 : Calculations = {
   tc : 6.25
 }
 const yarn5 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 2.4,
+  hdc : 3.5,
+  dc : 5,
+  tc : 7.25
 }
 const yarn6 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 2.6,
+  hdc : 3.5,
+  dc : 5,
+  tc : 8
 }
 const yarn7 : Calculations = {
-  sc : -1,
-  hdc : -1,
-  dc : -1,
-  tc : -1
+  sc : 4.5,
+  hdc : 6,
+  dc : 7.75,
+  tc : 10.75
 }
 
 const yarnCalcs : Record<number, Calculations>  = {
